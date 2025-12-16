a85914a979e25e168d286b19b8743e306edbbc07
Dario-git-hub <dariomiguelnavarro822@gmail.com>
2025-12-16 19:48:29 +0000

    Parte de fase2 hecha

--- FASE2.sh
+++ FASE2.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+# Instalar tailscale para tener acceso desde fuera de la WLAN
+curl -fsSL https://tailscale.com/install.sh | sh
+echo 'Realiza el login con tailscale. Cuando esté realizado, pulsa [ENTER] aquí'
+read
+
